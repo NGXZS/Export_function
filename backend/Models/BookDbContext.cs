57c161d295754678c18aef6c54ddab9a7846a72d
NGXZS <110776396+NGXZS@users.noreply.github.com>
2023-05-31 18:28:24 +0800

    Add files via upload

---
+++ backend/Models/BookDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PWAY_ASPNetCore_WebAPI.Models
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> BooksDB { get; set; } // MUST be DB name : get data from DB
+    }
+}
---
+++ backend/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace PWAY_ASPNetCore_WebAPI.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
