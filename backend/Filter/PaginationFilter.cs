311a5f66ec847d88ca6d8e096b3400266c7aad81
NGXZS <110776396+NGXZS@users.noreply.github.com>
2023-05-31 18:26:28 +0800

    Add files via upload

---
+++ backend/Filter/PaginationFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace PWAY_ASPNetCore_WebAPI.Filter
+{
+    public class PaginationFilter // set pagination LIMIT(size) & OFFSET(page)
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public PaginationFilter()
+        {
+            this.PageNumber = 1;
+            this.PageSize = 10;
+        }
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            this.PageNumber = pageNumber < 1 ? 1 : pageNumber; // min pageNumber = 1
+            this.PageSize = pageSize > 10 ? 10 : pageSize; //max PageSize = 10
+        }
+    }
+}
